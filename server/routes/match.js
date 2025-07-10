146b588ceac6b65e96cb4ee7a1e70970623fe64f
nannithin <nithinyalakala@gmail.com>
2025-07-10 14:52:13 +0530

    algo error fixed

--- server/routes/match.js
+++ server/routes/match.js
@@ -1,30 +1,39 @@
-const express = require("express")
-const router = express.Router()
-const Neighborhood = require("../models/Neighborhood")
+const express = require("express");
+const router = express.Router();
+const Neighborhood = require("../models/Neighborhood");
+
+function normalize(val, min, max) {
+  return (val - min) / (max - min);
+}
 
 router.post("/", async (req, res) => {
-  const prefs = req.body
+  const prefs = req.body;
+  const priorities = prefs.priorities;
+  const all = await Neighborhood.find();
 
-  const maxRent = 40000 
-  const all = await Neighborhood.find()
+  const rentMin = 10000;
+  const rentMax = 40000;
 
   const results = all.map(n => {
-    const priorities = prefs.priorities
+    const crimeScore = 1 - n.crimeRate;
+    const rentScore = 1 - normalize(n.rentAverage, rentMin, rentMax); 
+
     const score =
-      priorities.safety[0] * (1 - n.crimeRate) +
-      priorities.schools[0] * n.schoolRating +
-      priorities.nightlife[0] * n.nightlife +
-      priorities.parks[0] * n.parks +
-      priorities.shopping[0] * n.shopping +
-      priorities.restaurants[0] * n.restaurants +
-      priorities.publicTransit[0] * n.publicTransit +
-      priorities.walkability[0] * n.walkScore +
-      (10 - (n.rentAverage / maxRent) * 10) * 1  
+      crimeScore * priorities.safety[0] +
+      normalize(n.schoolRating, 0, 10) * priorities.schools[0] +
+      normalize(n.nightlife, 0, 10) * priorities.nightlife[0] +
+      normalize(n.parks, 0, 10) * priorities.parks[0] +
+      normalize(n.shopping, 0, 10) * priorities.shopping[0] +
+      normalize(n.restaurants, 0, 10) * priorities.restaurants[0] +
+      normalize(n.publicTransit, 0, 10) * priorities.publicTransit[0] +
+      normalize(n.walkScore, 0, 100) * priorities.walkability[0] +
+      rentScore * 10;
 
-    return { ...n._doc, matchScore: score }
-  })
+    return { ...n._doc, matchScore: score };
+  });
 
-  res.json(results.sort((a, b) => b.matchScore - a.matchScore).slice(0, 5)) 
-})
+  const sorted = results.sort((a, b) => b.matchScore - a.matchScore);
+  res.json(sorted.slice(0, 5)); 
+});
 
-module.exports = router
+module.exports = router;
